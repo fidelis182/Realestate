fa00adf672863230517dc424f3f3aad066330e64
fidelis182 <fedymueni18@gmail.com>
2025-05-21 15:41:19 +0300

    created the header section

--- realestate/src/components/header/Header.jsx
+++ realestate/src/components/header/Header.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import "./header.css";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <section className="h-wraper">
+        <div className=" flexCenter paddings innerWidth h-container">
+          <img src="./logo.png" alt="" width={100} />
+          <div className="flexCenter h-menu">
+            <a href="">Residencie</a>
+            <a href="">Our value</a>
+            <a href="">Contact Us</a>
+            <a href="">Get started</a>
+            <button className="button">
+              <a href="">Contact</a>
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 export default Header;
