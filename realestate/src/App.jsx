939729ccb0440d2d27e5b8b8813c106813163a15
fidelis182 <fedymueni18@gmail.com>
2025-05-21 15:03:33 +0300

    file

--- realestate/src/App.jsx
+++ realestate/src/App.jsx
@@ -1,4 +1,10 @@
+import Header from "./components/header/Header";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 export default App;
